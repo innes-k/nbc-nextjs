d8e64699b042ff1410f5a87958997b1525ad234b
innes-k <innes.kwak@gmail.com>
2024-03-12 01:50:34 +0900

    feat: ISR 작성

--- src/components/ISR.tsx
+++ src/components/ISR.tsx
@@ -1,7 +1,20 @@
+import { DataType } from "@/types/dataType";
 import React from "react";
 
-const ISR = () => {
-  return <div>ISR</div>;
+const ISR = async () => {
+  const response = await fetch(`https://catfact.ninja/fact`, {
+    next: {
+      revalidate: 5,
+    },
+  });
+  const data: DataType = await response.json();
+
+  return (
+    <div>
+      <li>fact : {data.fact}</li>
+      <li>length : {data.length}</li>
+    </div>
+  );
 };
 
 export default ISR;
--- src/components/SSG.tsx
+++ src/components/SSG.tsx
@@ -1,24 +1,34 @@
 import { DataType } from "@/types/dataType";
 import React, { useEffect, useState } from "react";
 
-const SSG = () => {
-  const [data, setData] = useState<DataType | null>(null);
+const SSG = async () => {
+  // 시도 1 - Warning 발생 (클라이언트와 서버에서 다른 값을 가져온다고 함)
+  // 오류 메시지 : Text content did not match. Server: "내용 1" Client: "내용 2"
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`https://catfact.ninja/fact`, {
-        cache: "force-cache",
-      });
-      const getData: DataType = await response.json();
-      setData(getData);
-    };
-    fetchData();
-  }, [data]);
+  const response = await fetch(`https://catfact.ninja/fact`, {
+    cache: "force-cache",
+  });
+  const data: DataType = await response.json();
+
+  // 시도 2 - Warning 해결 (이게 맞는지 모르겠어서 일단 주석처리)
+
+  // const [data, setData] = useState<DataType | null>(null);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch(`https://catfact.ninja/fact`, {
+  //       cache: "force-cache",
+  //     });
+  //     const getData: DataType = await response.json();
+  //     setData(getData);
+  //   };
+  //   fetchData();
+  // }, [data]);
 
   return (
     <div>
-      <li>fact : {data?.fact}</li>
-      <li>length : {data?.length}</li>
+      <li>fact : {data.fact}</li>
+      <li>length : {data.length}</li>
     </div>
   );
 };
