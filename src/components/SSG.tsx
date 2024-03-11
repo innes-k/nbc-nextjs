85d7c7f0a000de674597a149c01c9add639a974b
innes-k <innes.kwak@gmail.com>
2024-03-12 01:43:41 +0900

    feat: SSG 완료

--- src/components/SSG.tsx
+++ src/components/SSG.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { DataType } from "@/types/dataType";
+import React, { useEffect, useState } from "react";
 
 const SSG = () => {
-  return <div>SSG</div>;
+  const [data, setData] = useState<DataType | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://catfact.ninja/fact`, {
+        cache: "force-cache",
+      });
+      const getData: DataType = await response.json();
+      setData(getData);
+    };
+    fetchData();
+  }, [data]);
+
+  return (
+    <div>
+      <li>fact : {data?.fact}</li>
+      <li>length : {data?.length}</li>
+    </div>
+  );
 };
 
 export default SSG;
