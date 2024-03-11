e87fbc15c24b20a18003e83ea599eeff31b63ff6
innes-k <innes.kwak@gmail.com>
2024-03-12 01:53:56 +0900

    feat: SSR 작성

--- src/components/SSR.tsx
+++ src/components/SSR.tsx
@@ -1,7 +1,18 @@
+import { DataType } from "@/types/dataType";
 import React from "react";
 
-const SSR = () => {
-  return <div>SSR</div>;
+const SSR = async () => {
+  const response = await fetch(`https://catfact.ninja/fact`, {
+    cache: "no-cache",
+  });
+  const data: DataType = await response.json();
+
+  return (
+    <div>
+      <li>fact : {data.fact}</li>
+      <li>length : {data.length}</li>
+    </div>
+  );
 };
 
 export default SSR;
