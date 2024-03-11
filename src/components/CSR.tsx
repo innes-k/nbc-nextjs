b2cc09e99e631764b30bee373aba8a50dd69e5c2
innes-k <innes.kwak@gmail.com>
2024-03-12 01:58:54 +0900

    feat: CSR 작성

--- src/components/CSR.tsx
+++ src/components/CSR.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+import { DataType } from "@/types/dataType";
+import React, { useEffect, useState } from "react";
 
 const CSR = () => {
-  return <div>CSR</div>;
+  const [data, setData] = useState<DataType | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://catfact.ninja/fact`);
+      const getData = await response.json();
+      setData(getData);
+    };
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <li>fact : {data?.fact}</li>
+      <li>length : {data?.length}</li>
+    </div>
+  );
 };
 
 export default CSR;
